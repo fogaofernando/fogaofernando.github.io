6e6827cf03f2c9b3045515cc7122237f37b67943
fogaofernando <fogaofernando@gmail.com>
2017-07-12 18:23:20 -0300

    versão 1.2
    Correção definitiva do tema

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 /****Main****/
 var body = document.querySelector("body");
-var img3 = document.querySelector(".img3");
-var fotos = document.querySelectorAll(".fotos");
+
 
 
 if(objWidth(body)>800){
+    //***instancias
+    var img3 = document.querySelector(".img3");
+    var fotos = document.querySelectorAll(".fotos");
+    
+    //***aplicações
+    
     body.setAttribute("style","margin: auto; width: 80%;");
     
-    document.querySelector(".img3-paragrafo").setAttribute("style",String("width: "+objWidth(img3)+"px;"));
     
+    document.querySelector(".img3-paragrafo").setAttribute("style",String("width: "+objWidth(img3)+"px;")); 
     document.querySelector(".banner").innerHTML = "<img class='img-responsive' src='img/final.jpg' alt='' style='margin-left:auto;margin-right:auto;'>"
-    
     fotos.forEach(function(elemento, pos , array){
         elemento.setAttribute("style",String("margin-top: 20%;"));
     });
     
     
 }else{
-     document.querySelector(".banner").innerHTML = "<img class='img-responsive' src='img/coracao.png' alt='' style='margin-left:auto;margin-right:auto; margin-bottom: 100px;'>"
+    //***instancias
+    var introHead = document.querySelector(".col-lg-12");
+    
+    
+    //***aplicações
+    introHead.setAttribute("style","margin-top: 80%;");
+    
+    document.querySelector(".banner").innerHTML = "<img class='img-responsive' src='img/coracao.png' alt='' style='margin-left:auto;margin-right:auto; margin-bottom: 100px;'>"
 }
 
 
